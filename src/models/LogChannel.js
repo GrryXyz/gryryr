6d8c4410d8cb5ba3a7098f1d874c975c12c02c4d
GrryXyz <greyvaldy16@gmail.com>
2026-02-06 15:23:16 +0800

    LogChannel.js

--- src/models/LogChannel.js
+++ src/models/LogChannel.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongo");
+const { Schema, model } = require("mongoose");
 
 module.exports = model("LogChannel", new Schema({
   guildId: { type: String, unique: true },
